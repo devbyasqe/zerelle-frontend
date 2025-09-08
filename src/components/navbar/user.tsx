c310a7b15aae4c36c430028b74547a805ffb5cca
devbyasqe <connectashiques@gmail.com>
2025-09-08 08:01:24 +0530

    optimization

--- src/components/navbar/user.tsx
+++ src/components/navbar/user.tsx
@@ -1,34 +1,60 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
-import { CartIcon, MenuIcon, SearchIcon, WishlistIcon } from "../svg";
+import { CartIcon, MenuIcon, SearchIcon, WishlistIcon, XIcon } from "../svg";
+import Modal from "../ui/modal";
 
 const UserNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="fixed top-0 inset-x-0 z-50 isolate min-w-80 w-[95%] sm:max-md:max-w-[40rem] max-w-[90rem] mx-auto ">
-      <nav className="h-14 flex items-center justify-between border-b border-r border-l bg-background/50 backdrop-blur">
-        <Link href={"/"} className="inline-flex text-xl font-semibold px-4">
-          Zerelle
-        </Link>
-        <div className="flex gap-2 pe-4">
-          <Button size={"icon"} variant={"accent"}>
-            <SearchIcon />
-          </Button>
-          <Button size={"icon"} variant={"accent"}>
-            <CartIcon />
-          </Button>
-          <Button size={"icon"} variant={"accent"}>
-            <WishlistIcon />
-          </Button>
-          <Button size={"icon"} variant={"accent"}>
-            <MenuIcon />
+    <>
+      <header className="fixed top-0 inset-x-0 z-50 isolate min-w-80 w-[95%] sm:max-md:max-w-[40rem] max-w-[90rem] mx-auto ">
+        <nav className="h-14 flex items-center justify-between border-b border-r border-l bg-background/50 backdrop-blur">
+          <Link href={"/"} className="inline-flex text-xl font-semibold px-4">
+            Zerelle
+          </Link>
+          <div className="flex gap-2 pe-4">
+            <Button size={"icon"} variant={"accent"}>
+              <SearchIcon />
+            </Button>
+            <Button size={"icon"} variant={"accent"}>
+              <CartIcon />
+            </Button>
+            <Button size={"icon"} variant={"accent"}>
+              <WishlistIcon />
+            </Button>
+            <Button
+              size={"icon"}
+              variant={"accent"}
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            >
+              <MenuIcon />
+            </Button>
+          </div>
+        </nav>
+      </header>
+      <Modal
+        isOpen={isOpen}
+        centerChild={false}
+        className="max-w-md ms-auto h-full"
+      >
+        <div className="py-2 pe-4 flex justify-end">
+          <Button
+            size={"icon"}
+            variant={"accent"}
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          >
+            <XIcon />
           </Button>
         </div>
-      </nav>
-    </header>
+      </Modal>
+    </>
   );
 };
 
 export default UserNavbar;
-
- 
--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -3,20 +3,29 @@ import React from "react";
 type TModal = React.HTMLAttributes<HTMLElement> & {
   isOpen: boolean;
   isCenter?: boolean;
+  centerChild?: boolean;
 };
 
-const Modal = ({ isOpen, isCenter, className, ...props }: TModal) => {
+const Modal = ({
+  isOpen,
+  isCenter,
+  centerChild = true,
+  className,
+  ...props
+}: TModal) => {
   return (
     isOpen && (
       <section
         className={cn(
-          "fixed z-50 inset-0  sm:max-md:max-w-[40rem] max-w-[90rem] mx-auto transition-all duration-300 py-4 bg-accent/50  backdrop-blur overflow-y-auto",
-          isCenter && "flex items-center justify-center"
+          "fixed z-50 inset-0 w-[95%] sm:max-md:max-w-[40rem] max-w-[90rem] mx-auto transition-all duration-300  bg-accent/50  backdrop-blur overflow-y-auto",
+          isCenter && "flex items-center justify-center",
+          centerChild ? "py-4 ":"py-1"
         )}
       >
         <div
           className={cn(
-            "relative w-[92%] mx-auto rounded-xl p-4 bg-background border",
+            "relative rounded-xl  bg-background border",
+            centerChild && "mx-auto p-4",
             className
           )}
           {...props}
